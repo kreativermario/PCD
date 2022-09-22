243e4f0669efa85684daf1ce6ab1e3adf664c518
kreativermario <80268327+kreativermario@users.noreply.github.com>
2022-09-22 17:31:56 +0100

    Semana 2 - Exercicios

--- src/Sem2/Car.java
+++ src/Sem2/Car.java
@@ -1,7 +1,10 @@
 package Sem2;
 import java.util.Observable;
+import java.util.Random;
 
-public class Car extends Observable {
+import static java.lang.Thread.sleep;
+
+public class Car extends Observable implements Runnable {
 	private int id;
 	private int limit;
 	private Thread thread;
@@ -28,12 +31,28 @@ public class Car extends Observable {
 		this.notifyObservers();
 	}
 
+	@Override
+	public void run() {
+		try{
+			for(int i = 0; i< limit; i++) {
+				Random r = new Random();
+				int low = 0;
+				int high = 100;
+				int result = r.nextInt(high - low) + low;
+				increasePosition();
+				sleep(result);
+			}
+		} catch (InterruptedException e) {
+
+		}
+
+	}
+
 	public Car(int id, int limit) {
 		super();
 		this.id = id;
 		this.limit = limit;
-		MyRunnable mr = new MyRunnable(this, limit);
-		Thread thread1 = new Thread(mr);
+		Thread thread1 = new Thread(this);
 		this.thread = thread1;
 		thread1.start();
 	}
