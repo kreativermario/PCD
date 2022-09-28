45bdcf4f5e27bc3bb245ec4988fc234ef799f58d
kreativermario <80268327+kreativermario@users.noreply.github.com>
2022-09-28 20:57:58 +0100

    Semana 3 - Exercicios

--- src/Sem3/Account.java
+++ src/Sem3/Account.java
@@ -51,39 +51,15 @@ public class Account {
 
     public static void main(String[] args) {
         Account account = new Account();
-        Client c1 = new Client(account);
-        Client c2 = new Client(account);
-        Client c3 = new Client(account);
-        Client c4 = new Client(account);
-        Client c5 = new Client(account);
-        Client c6 = new Client(account);
-        Client c7 = new Client(account);
-        Client c8 = new Client(account);
-        Client c9 = new Client(account);
-        Client c10 = new Client(account);
         Client [] clients = new Client[10];
-        clients[0] = c1;
-        clients[1] = c2;
-        clients[2] = c3;
-        clients[3] = c4;
-        clients[4] = c5;
-        clients[5] = c6;
-        clients[6] = c7;
-        clients[7] = c8;
-        clients[8] = c9;
-        clients[9] = c10;
         int totalDeposited = 0;
         try{
-            c1.start();
-            c2.start();
-            c3.start();
-            c4.start();
-            c5.start();
-            c6.start();
-            c7.start();
-            c8.start();
-            c9.start();
-            c10.start();
+            for(int i = 0; i< 10; i++){
+                Client c = new Client(account);
+                clients[i] = c;
+                c.start();
+            }
+
             sleep(10000);
             for(Client c : clients){
                 totalDeposited += c.getTotalDeposited();
