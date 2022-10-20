2dce9f20e74eac3681d131c890bd05917f1d90b1
kreativermario <80268327+kreativermario@users.noreply.github.com>
2022-10-20 15:36:07 +0100

    Semana 6 - Exercicios com comentários

--- src/Sem6/Ex2Latch/SearcherThread.java
+++ src/Sem6/Ex2Latch/SearcherThread.java
@@ -1,20 +1,19 @@
 package Sem6.Ex2Latch;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.CountDownLatch;
 
 
 public class SearcherThread extends Thread{
     private String myText;
     private String textToFind;
-    private CyclicBarrier barrier;
+    private CountDownLatch latch;
     private int result=-1;
 
     public SearcherThread(String myText, String textToFind,
-                          CyclicBarrier barrier) {
+                          CountDownLatch latch) {
         this.myText = myText;
         this.textToFind = textToFind;
-        this.barrier = barrier;
+        this.latch = latch;
     }
 
     public String getMyText() {
@@ -27,13 +26,16 @@ public class SearcherThread extends Thread{
 
     @Override
     public void run() {
+        long startTime = System.nanoTime();
+
         result=myText.indexOf(textToFind);
-        try {
-            barrier.await();
-            System.err.println("Thread finishing at:"+System.currentTimeMillis());
-        } catch (InterruptedException |
-                 BrokenBarrierException e) {
-        }
+
+        latch.countDown();
+
+        long endTime = System.nanoTime();
+        long time = (long) ((endTime - startTime)/(1e6));
+        System.err.println("Thread finishing at:" + time);
+
     }
 
 
