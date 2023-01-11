41d4ba4e04f17d317c83a4e5fef88be3976903ef
kreativermario <80268327+kreativermario@users.noreply.github.com>
2023-01-11 15:48:04 +0000

    Correcao exercicio Distributor.java - Sem4

--- src/Sem4/Distributor.java
+++ src/Sem4/Distributor.java
@@ -10,7 +10,7 @@ import java.util.Random;
 
 public class Distributor {
 
-    private final Supplier supplier = new Supplier();
+    private final Supplier supplier;
     private final Client [] clients;
     public static final int CAPACITY = 10;
     private LinkedList<Integer> listaProdutos;
@@ -20,9 +20,13 @@ public class Distributor {
         this.textField = field;
         listaProdutos = new LinkedList<Integer>();
         this.clients = new Client[num_clients];
+        this.supplier = new Supplier();
         for(int i = 0; i < clients.length; i++){
-            clients[i] = new Client();
+            Client c = new Client();
+            clients[i] = c;
+            c.start();
         }
+        supplier.start();
         // escreve-se os conteúdos da lista no campo de texto assim...
         textField.setText(listaProdutos.toString());
     }
@@ -39,16 +43,11 @@ public class Distributor {
     }
 
     /**
-     * Método que vai colocar o produto
+     * Método que vai colocar o produto sucessivamente
      * @param product Produto
      * @throws InterruptedException wait()
      */
     public synchronized void put(Integer product) throws InterruptedException {
-        if(listaProdutos.size() >= CAPACITY){
-            System.out.println("Supplier " + Thread.currentThread().toString() + " - fila está cheia. [ESPERANDO]"
-                    + " SIZE: " + listaProdutos.size());
-            wait();
-        }
         this.listaProdutos.add(product);
         System.out.println("Supplier " + Thread.currentThread().toString() + " inseri produto [" + product + "]"
                 + " SIZE: " + listaProdutos.size());
@@ -61,10 +60,9 @@ public class Distributor {
      * @return finalProducts Produtos a devolver
      * @throws InterruptedException wait()
      */
-
     //PROBLEMAS POIS NAO SEI, RETIRA QUANDO NAO ESTA 10 ACIMA...
     public synchronized LinkedList<Integer> take() throws InterruptedException {
-        if(listaProdutos.isEmpty() || listaProdutos.size() < CAPACITY){
+        while(listaProdutos.isEmpty() || listaProdutos.size() < CAPACITY){
             System.out.println("RETALHISTA + " + Thread.currentThread().toString() + " - não tem produtos [ESPERANDO]");
             wait();
         }
@@ -81,58 +79,32 @@ public class Distributor {
 
     public class Supplier extends Thread{
 
-        public Supplier(){
-            Thread t = new Thread(this);
-            System.out.println("Supplier " + Thread.currentThread().toString() + " - inicio do run.");
-            t.start();
-        }
-
         @Override
         public void run(){
+            System.out.println("Supplier " + Thread.currentThread().toString() + " - inicio do run.");
             try{
                 while(true) {
-                    if (this.isInterrupted()) {
-                        System.out.println("SUPPLIER + " + Thread.currentThread().toString() + " - interrupted");
-                        break;
-                    }
-                    int min = 0;
-                    int max = 9;
-                    Random random = new Random();
-                    int randomnumber = random.nextInt(max - min) + min;
-
+                    sleep(500);
+                    int randomnumber = (int) (0 + Math.random()*(9-0));
                     put(randomnumber);
-
                 }
             } catch (InterruptedException e) {
                 System.out.println("SUPPLIER + " + Thread.currentThread().toString() + " - interrupted");
             }
-
         }
-
     }
 
 
     public class Client extends Thread{
 
         private int numBought = 0;
-        private LinkedList<Integer> boughtItems = new LinkedList<Integer>();
-
-        public Client(){
-            Thread t = new Thread(this);
-            System.out.println("Retalhista " + Thread.currentThread().toString() + " - inicio do run.");
-            t.start();
-        }
+        private LinkedList<Integer> boughtItems = new LinkedList<>();
 
         @Override
         public void run(){
-
+            System.out.println("Retalhista " + Thread.currentThread().toString() + " - inicio do run.");
             try{
                 while(true) {
-                    if(this.isInterrupted()){
-                        System.out.println("RETALHISTA + " + Thread.currentThread().toString() + " - comprei um total de "
-                                + numBought + " lotes");
-                        break;
-                    }
                     sleep(500);
                     boughtItems.addAll(take());
                     numBought++;
@@ -141,7 +113,6 @@ public class Distributor {
             } catch (InterruptedException e) {
                 System.out.println("RETALHISTA + " + Thread.currentThread().toString() + " - comprei um total de "
                         + numBought + " lotes");
-
             }
 
         }
@@ -150,32 +121,51 @@ public class Distributor {
 
     }
 
+    public static class GUI{
+        private JFrame frame;
+
+
+        public GUI(){
+            frame = new JFrame("Distribuição Pingo Doce");
+            addFrame();
+
+            // para que o botao de fechar a janela termine a aplicacao
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            // para que a janela se redimensione de forma a ter todo o seu conteudo visivel
+            frame.pack();
+        }
+
+        private void addFrame(){
+            JPanel panel = new JPanel(new GridLayout(2,1));
+            JTextField textfield = new JTextField();
+            JButton button = new JButton("STOP");
+            Distributor distributor = new Distributor(textfield, 4);
+            button.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    distributor.stop();
+                }
+            });
+            panel.add(textfield);
+            panel.add(button);
+            frame.add(panel);
+        }
+
+        public void open(){
+            frame.setVisible(true);
+        }
+
+
+
+
+    }
+
+
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Distribuição Pingo Doce");
-        JPanel panel = new JPanel(new GridLayout(2,1));
-        JTextField textfield = new JTextField();
-        JButton button = new JButton("STOP");
-        Distributor distributor = new Distributor(textfield, 4);
-
-
-        button.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                distributor.stop();
-            }
-        });
-        panel.add(textfield);
-        panel.add(button);
-        frame.add(panel);
-
-        // para que o botao de fechar a janela termine a aplicacao
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        // para que a janela se redimensione de forma a ter todo o seu conteudo visivel
-        frame.pack();
-        frame.setVisible(true);
+        GUI gui = new GUI();
+        gui.open();
 
     }
 
